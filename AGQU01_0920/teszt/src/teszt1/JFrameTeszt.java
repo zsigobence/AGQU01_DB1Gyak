363e729c76b7ffb0eb808b90d4938555b340bb46
zsigobence <zsigobence@gmail.com>
2022-10-25 11:29:58 +0200

    .

---
+++ AGQU01_0920/teszt/src/teszt1/JFrameTeszt.java
@@ -0,0 +1,26 @@
+package teszt1;
+
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class JFrameTeszt {
+	public static void main(String s[]) {
+		JFrame frame = new JFrame("JFrame példa");
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		JLabel label = new JLabel("JFrame példa");
+		JButton button = new JButton();
+		button.setText("Button");
+		panel.add(label);
+		panel.add(button);
+		frame.add(panel);
+		frame.setSize(200, 300);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		}
+	}
+
